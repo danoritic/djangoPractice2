6c95b7462269e4f4ed5180023718870112bd277e
Olasiyan Daniel <danoritic@gmail.com>
2025-02-14 02:44:04 +0100

    fresh bata

--- familyapp/urls.py
+++ familyapp/urls.py
@@ -1,1 +1,12 @@
-urlpatterns=[]
+from django.urls import path
+
+from familyapp.views import member_list, member_detail
+
+
+urlpatterns=[
+    path("members/", member_list(), name="member_list"),
+    # path("members/create", member_detail(), name="create_member"),
+    
+    
+    path("members/<int:pk>/", member_detail(), name="member_detail")
+]
--- familyapp/views.py
+++ familyapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from familyapp.models import Member
+def member_list(request): 
+    MAX_OBJECTS = 20
+    member = Member.objects.all()[:MAX_OBJECTS]
+    data = {"results": list(member.values("name", "alias", "date_of_birth"))}
+    return JsonResponse(data)
+
+
+def member_detail(request, pk):
+    member = get_object_or_404(Member, pk=pk) 
+    data = {"results": {
+            "name": member.name,
+            "alias": member.alias,
+            "date_of_birth": member.date_of_birth
+    }}
+    return JsonResponse(data)
